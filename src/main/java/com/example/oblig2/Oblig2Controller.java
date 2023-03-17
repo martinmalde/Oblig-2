a50d1e10f71f8a2a44612c7ab2269f60055be656
martinmalde <s376604@oslomet.no>
2023-03-17 11:47:01 +0100

    Obligen 2

---
+++ src/main/java/com/example/oblig2/Oblig2Controller.java
@@ -0,0 +1,30 @@
+package com.example.oblig2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class Oblig2Controller {
+
+    private final List<Billetter> alleBilletter = new ArrayList<>();
+
+    @PostMapping("/lagre")
+    public void lagreBillett(Billetter innBillett) {
+        alleBilletter.add(innBillett);
+    }
+
+    @GetMapping("/hentAlle")
+    public List<Billetter> hentAlle(){
+        return alleBilletter;
+    }
+
+    @GetMapping("/slettBillettene")
+    public void slettBillettene(){ alleBilletter.clear(); }
+
+
+
+}
---
+++ src/main/resources/static/index.js
@@ -0,0 +1,93 @@
+function registrer(){
+
+    const billett = {
+        film: $('#film').val(),
+        antall: $('#antall').val(),
+        fornavn: $("#fornavn").val(),
+        etternavn: $("#etternavn").val(),
+        telefonnr: $("#telefonnr").val(),
+        epost: $("#epost").val()
+    };
+    let feil = 0;
+
+    if($('#film').val() === "" || $('#film').val() === 0 || $("#film").val() === "Default"){
+        document.getElementById("antall.feil").style.display = "inline";
+        feil++;
+    } else {
+        document.getElementById("antall.feil").style.display = "none";
+    }
+    if($('#antall').val() === "" || $('#antall').val() === 0){
+        document.getElementById("antall.feil").style.display = "inline";
+        feil++;
+    } else {
+        document.getElementById("antall.feil").style.display = "none";
+    }
+
+    if($('#fornavn').val() === "" || $('#fornavn').val() === 0){
+        document.getElementById("fornavn.feil").style.display = "inline";
+        feil++;
+    } else {
+        document.getElementById("fornavn.feil").style.display = "none";
+    }
+
+    if($('#etternavn').val() === "" || $('#etternavn').val() === 0){
+        document.getElementById("etternavn.feil").style.display = "inline";
+        feil++;
+    } else {
+        document.getElementById("etternavn.feil").style.display = "none";
+    }
+
+    if($('#telefonnr').val() === "" || $('#telefonnr').val() === 0){
+        document.getElementById("telefonnr.feil").style.display = "inline";
+        feil++;
+    } else {
+        document.getElementById("telefonnr.feil").style.display = "none";
+    }
+
+    if($('#epost').val() === "" || $('#epost').val() === 0){
+        document.getElementById("epost.feil").style.display = "inline";
+        feil++;
+    } else {
+        document.getElementById("epost.feil").style.display = "none";
+    }
+
+    // dersom en eller flere felter er tomme, returerer den false
+    if(feil > 0){
+        return false;
+    } else {
+
+    $.post("/lagre", billett, function (){
+        hentAlle();
+    });
+
+
+    $("#film").val(""),
+        $("#antall").val(""),
+        $("#fornavn").val(""),
+        $("#etternavn").val(""),
+        $("#telefonnr").val(""),
+        $("#epost").val("");
+}
+}
+
+function hentAlle() {
+    $.get("/hentAlle", function (data){
+        formaterData(data);
+    });
+}
+
+function formaterData(billetter){
+    let ut = "<table class='table table-striped table-bordered'><tr><th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th></tr>";
+    for (const billett of billetter){
+        ut += "<tr><td>"+billett.film+"</td><td>"+billett.antall+"</td><td>"+billett.fornavn+"</td><td>"+billett.etternavn+"</td><td>"+billett.telefonnr+"</td><td>"+billett.epost+"</td></tr>"
+    }
+    ut += "</table>";
+    $("#billettRegister").html(ut);
+}
+
+function slettBillettene(){
+    $.get("/slettBillettene", function (){
+        hentAlle();
+    })
+
+}
